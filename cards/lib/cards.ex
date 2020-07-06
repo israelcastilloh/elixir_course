bca34ed937107bf9683ab32863187660fc09bdbd
israelcastilloh <castillo.israelh@gmail.com>
2020-07-06 14:03:52 -0500

    create_deck

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -2,6 +2,17 @@ defmodule Cards do
 
   def hello do
     "hi there!"
+
   end
 
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+
 end
+
+#Cards.hello can be call in shell as a method.
+#Implicit return, no return whatsoever
+#Single quotes is supported but better ""
+#recompile to recompile
